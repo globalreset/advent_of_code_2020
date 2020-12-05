41ca16275406613ac18c868bc65dcbc6d87b636c
globalreset <shughes@pobox.com>
2020-12-05 02:55:05 -0600

    couldn't help but make it a little bit smaller when I realized something about the seatId

--- day5/dayfive.rb
+++ day5/dayfive.rb
@@ -1,29 +1,13 @@
 
-seatBinValues = {
-  'F' => 0,
-  'B' => 1,
-  'L' => 0,
-  'R' => 1
-}
-seatList = []
-IO.readlines("day5_input.txt").each { |line|
-  row = 0
-  col = 0
-  if(line.size()>0)
-     codes = line.scan(/\w/)
-     codes[0..6].each { |code| row = (row<<1) + seatBinValues[code] }
-     codes[7..9].each { |code| col = (col<<1) + seatBinValues[code] }
-     seatList.push (row*8 + col)
-  end
-}
-seatList.sort!
+seatBinValues = {'F' => 0,  'B' => 1, 'L' => 0, 'R' => 1}
+seatList = IO.readlines("day5_input.txt").collect { |line|
+   line.scan(/\w/).collect { |code| seatBinValues[code] }.join("").to_i(2)
+}.sort
+
 puts "Part 1 answer: highest seatId = #{seatList[-1]}"
 
-seatList.size.times { |i|
-   if(i>=1)
-     if(seatList[i]-2==seatList[i-1])
-        puts "Part 2 answer: missing seatId = #{seatList[i]-1}"
-        exit
-     end
-  end
+(1..(seatList.size()-1)).each { |i|
+   if(seatList[i]-2==seatList[i-1])
+      puts "Part 2 answer: missing seatId = #{seatList[i]-1}"
+   end
 }
