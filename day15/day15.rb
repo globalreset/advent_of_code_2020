a6fd160e27a174607ba2f8668db0f4f728edf47d
globalreset <shughes@pobox.com>
2020-12-14 23:36:26 -0600

    couldn't help cleaning it up a bit

--- day15/day15.rb
+++ day15/day15.rb
@@ -1,22 +1,11 @@
 numList = [13,16,0,12,15,1]
-#numList = [0,3,6]
 lastTurn = {}
 prevAge = 0
 num = 0
 #2020.times { |i|
 30000000.times { |i|
-  if(i<numList.size())
-    num = numList[i%numList.size]
-  else
-    num = prevAge
-  end
-  #puts "#{i+1}: #{num}"
-  if(lastTurn[num]==nil)
-    lastTurn[num] = i
-    prevAge = 0
-  else
-    prevAge = i - lastTurn[num]
-    lastTurn[num] = i
-  end
+  num = numList.shift || prevAge
+  prevAge = lastTurn[num] ? i-lastTurn[num] : 0
+  lastTurn[num] = i
 }
 puts num
