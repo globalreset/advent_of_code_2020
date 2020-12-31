28258cbd9a2901503578bbfadbca44704de3475e
globalreset <shughes@pobox.com>
2020-12-30 19:56:08 -0600

    some cleanup

--- day23/day23_part2_linkedlist.rb
+++ day23/day23_part2_linkedlist.rb
@@ -5,14 +5,10 @@ map = {}
 cups = input.scan(/\d/).map(&:to_i)
 cups = [cups, (10..1_000_000).to_a].flatten
 maxCup = cups.max
-prevCup = -1
-cups.each{ |cup|
-  cupPtr = LinkedList.new(cup, nil)
-  if(prevCup>0)
-    map[prevCup].next = cupPtr
-  end
-  map[cup] = cupPtr
-  prevCup = cup
+cups.each_cons(2) { |a,b|
+  map[a] ||= LinkedList.new(a, nil)
+  map[b] ||= LinkedList.new(b, nil)
+  map[a].next = map[b]
 }
 map[cups[-1]].next = map[cups[0]]
 currentCup = map[cups[0]]
@@ -26,11 +22,9 @@ currentCup = map[cups[0]]
     dst -= 1
     dst = maxCup if(dst<=0)
   end
-  tmpDstNext = map[dst].next
+  pickup.next.next.next = map[dst].next
   map[dst].next = pickup
-  pickup.next.next.next = tmpDstNext
   currentCup = currentCup.next
 }
 
-puts "#{map[1].next.value} * #{map[1].next.next.value} = "
-puts "#{map[1].next.value  *   map[1].next.next.value} "
+puts "#{map[1].next.value} * #{map[1].next.next.value} = #{map[1].next.value*map[1].next.next.value} "
